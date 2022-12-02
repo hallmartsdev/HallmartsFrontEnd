bc7bf100d4446aa437610f5e578980253c7a7e4e
Ekweaga <hikay133@gmail.com>
2022-12-02 13:04:11 +0100

    three components created

---
+++ components/Homepage/SeasonCollections.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SeasonCollections() {
+  return (
+    <div>SeasonCollections</div>
+  )
+}
+
+export default SeasonCollections
---
+++ components/Homepage/WeekHighlights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WeekHighlights() {
+  return (
+    <div>WeekHighlights</div>
+  )
+}
+
+export default WeekHighlights
